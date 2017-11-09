907d36f09f8308e249bedf19cccf00d9ebf0a162
nguyendangkhoa25 <nguyendangkhoa25@gmail.com>
2017-11-09 22:26:07 +0700

    test

--- SpringSecurityRememberMeAnnotationExample/src/main/java/com/khoanguyen/springsecurity/controller/HelloWorldController.java
+++ SpringSecurityRememberMeAnnotationExample/src/main/java/com/khoanguyen/springsecurity/controller/HelloWorldController.java
@@ -18,7 +18,7 @@ public class HelloWorldController {
 	
 	@RequestMapping(value = { "/", "/home" }, method = RequestMethod.GET)
 	public String homePage(ModelMap model) {
-		model.addAttribute("greeting", "Hi, Welcome to my website");
+		model.addAttribute("greeting", "Hi, Welcome to mysite");
 		return "welcome";
 	}
 
