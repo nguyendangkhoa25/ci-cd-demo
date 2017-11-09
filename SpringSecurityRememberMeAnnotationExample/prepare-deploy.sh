e3d4d04edc1f72a008a9a355d6a1d5a51c7eeca8
Khoa Nguyen Dang <nguyendangkhoa25@gmail.com>
2017-11-09 21:40:47 +0700

    add deploy step

--- SpringSecurityRememberMeAnnotationExample/prepare-deploy.sh
+++ SpringSecurityRememberMeAnnotationExample/prepare-deploy.sh
@@ -6,8 +6,8 @@ source /etc/environment
 echo "------------------- [BEGIN] Clean old Docker Container ---------------------"
 DOCKER_CONTAINER_ID="$1"
 
-docker stop ${DOCKER_CONTAINER_ID} > /dev/null
-docker rm ${DOCKER_CONTAINER_ID} > /dev/null
+docker stop ${DOCKER_CONTAINER_ID} 2> /dev/null
+docker rm ${DOCKER_CONTAINER_ID} 2> /dev/null
 
 echo "------------------- [END] Clean old Docker Container ---------------------"
 exit 0
